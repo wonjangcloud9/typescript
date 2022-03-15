6549a20b2aadce4399ad95d4314ede542478109a
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-03-15 23:51:31 +0900

    custom validation

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -36,6 +36,7 @@ interface IForm {
   username: string;
   password: string;
   password1: string;
+  extraError?: string;
 }
 
 function ToDoList() {
@@ -43,14 +44,26 @@ function ToDoList() {
     register,
     watch,
     handleSubmit,
+    setError,
     formState: { errors },
   } = useForm<IForm>({
     defaultValues: {
       email: "@naver.com",
     },
   });
-  const onValid = (data: any) => {
-    console.log(data);
+  const onValid = (data: IForm) => {
+    if (data.password !== data.password1) {
+      setError(
+        "password1",
+        { message: "Password are not the same" },
+        { shouldFocus: true }
+      );
+    }
+    // setError(
+    //   "extraError",
+    //   { message: "Server offline." },
+    //   { shouldFocus: true }
+    // );
   };
   console.log(errors);
   return (
@@ -71,7 +84,15 @@ function ToDoList() {
         />
         <span>{errors?.email?.message}</span>
         <input
-          {...register("firstName", { required: "write here" })}
+          {...register("firstName", {
+            required: "write here",
+            validate: {
+              noNico: (value) =>
+                value.includes("nico") ? "no nicos allowed" : true,
+              noNick: (value) =>
+                value.includes("nick") ? "no nicks allowed" : true,
+            },
+          })}
           placeholder="First Name"
         />
         <span>{errors?.firstName?.message}</span>
@@ -99,6 +120,7 @@ function ToDoList() {
         />
         <span>{errors?.password1?.message}</span>
         <button>Add</button>
+        <span>{errors?.extraError?.message}</span>
       </form>
     </div>
   );
