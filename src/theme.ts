3348b2d45af43f7bedbc24e0214c4655b7f829fc
wonjangcloud9 <48057918+wonjangcloud9@users.noreply.github.com>
2022-03-17 14:56:07 +0900

    style placeholder dnd

--- src/theme.ts
+++ src/theme.ts
@@ -1,8 +1,7 @@
 import { DefaultTheme } from "styled-components";
 
 export const darkTheme: DefaultTheme = {
-  bgColor: "#2f3640",
-  textColor: "black",
-  accentColor: "#44bd32",
-  cardBgColor: "transparent",
+  bgColor: "#3F8CF2",
+  boardColor: "#DADFE9",
+  cardColor: "white",
 };
